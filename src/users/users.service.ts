3da415e0aa7ac989cd3de4c5216b207bba3b42d7
Nikolaj <samsung831@gmail.com>
2024-11-19 20:04:07 +0100

    added sqlite DB and API for users with entity

---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
+  create(createUserDto: CreateUserDto) {
+    const newUser = this.userRepository.create(createUserDto);
+
+    return this.userRepository.save(newUser);
+  }
+
+  findAll() {
+    return this.userRepository.find(); //select * from user
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+
+    return this.userRepository.save({ ...user, ...updateUserDto});
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    return this.userRepository.remove(user);
+  }
+}
